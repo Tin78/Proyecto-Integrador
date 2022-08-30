96139157936112318e821991df3afe98e3bd4671
Tin78 <marocar95@hotmail.com>
2022-08-30 10:31:56 -0300

    7th commit
    Created Backend folder with Spring Boot initializr

---
+++ Proyecto_RomeraCarmona/Backend/src/main/java/com/portfolio/Tin78/Tin78Application.java
@@ -0,0 +1,13 @@
+package com.portfolio.Tin78;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tin78Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tin78Application.class, args);
+	}
+
+}
---
+++ Proyecto_RomeraCarmona/Backend/src/test/java/com/portfolio/Tin78/Tin78ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.Tin78;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tin78ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
