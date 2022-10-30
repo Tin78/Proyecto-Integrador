21b4781ce31d0f6bbed4a467055b1cf3ccd89d5b
Tin78 <marocar95@hotmail.com>
2022-10-30 13:54:37 -0300

    12th commit

--- Proyecto_RomeraCarmona/Backend/src/main/java/com/PI/Tin78/Controller/PersonaController.java
+++ Proyecto_RomeraCarmona/Backend/src/main/java/com/PI/Tin78/Controller/PersonaController.java
@@ -4,6 +4,7 @@ import com.PI.Tin78.Entity.Persona;
 import com.PI.Tin78.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,20 +25,23 @@ public class PersonaController {
         return ipersonaService.getPersona();
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("personas/crear")
     public String createPersona(@RequestBody Persona persona){
         ipersonaService.savePersona(persona);
         return "La persona fue creada exitosamente";
     }
     
-    @DeleteMapping("/personas/borrar/(id)")
+    @PreAuthorize("hasRole('ADMIN')")
+    @DeleteMapping("/personas/borrar/{id}")
     public String deletePersona(@PathVariable Long id){
         ipersonaService.deletePersona(id);
         return "La persona fue borrada exitosamente";
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     // Por ej: localhost:8080/personas/editar/4?nombre=Lucía&apellido=Giménez&img=foto_1
-    @PutMapping("/personas/editar/(id)")
+    @PutMapping("/personas/editar/{id}")
     public Persona editPersona(@PathVariable Long id,
             @RequestParam("nombre") String nuevoNombre,
             @RequestParam("apellido") String nuevoApellido,
--- Proyecto_RomeraCarmona/Frontend/src/app/model/persona.model.ts
+++ Proyecto_RomeraCarmona/Frontend/src/app/model/persona.model.ts
@@ -1,10 +1,10 @@
 export class persona {
   id?: number;
-  nombre!: String;
-  apellido!: String;
-  img!: String;
+  nombre!: string;
+  apellido!: string;
+  img!: string;
 
-  contructor(nombre: String, apellido: String, img: String) {
+  contructor(nombre: string, apellido: string, img: string) {
     this.nombre = nombre;
     this.apellido = apellido;
     this.img = img;
